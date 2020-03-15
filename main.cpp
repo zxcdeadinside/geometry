f35279f408b28da8c1c68c874e6267f2728ab106
Nikita <bidros2002@mail.ru>
2020-03-16 04:40:05 +0700

    Final code written

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 #include <cmath>
-#define PI 3.14
+#define PI 3.1415926
 #include <iostream>
 
 using namespace std;
@@ -8,7 +8,7 @@ void circle(double x0, double y0, double rad)
 {
     double perimeter0, area0;
     if (rad < 0)
-        cout << "Error: invalid radius input";
+        cout << "Error: invalid radius input" << endl;
     else {
         perimeter0 = 2 * PI * rad;
         area0 = PI * pow(rad, 2);
@@ -18,46 +18,42 @@ void circle(double x0, double y0, double rad)
         return;
     }
 }
-void triangle(double x1, double y1, double x2, double y2, double x3, double y3, double x4, double y4)
+void triangle(double x1, double y1, double x2, double y2, double x3, double y3)
 {
     double perimeter, area;
     double a, b, c, sin_ab, temp1, temp2, pol;
 
-    temp1 = x2 - x1;
-    temp2 = y2 - y1;
-    a = sqrt(pow(temp1, 2) + pow(temp2, 2));
-    temp1 = x3 - x2;
-    temp2 = y3 - y2;
-    b = sqrt(pow(temp1, 2) + pow(temp2, 2));
-    temp1 = x3 - x4;
-    temp2 = y3 - y4;
-    c = sqrt(pow(temp1, 2) + pow(temp2, 2));
+    a = sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
+    b = sqrt((x3-x1)*(x3-x1) + (y3-y1)*(y3-y1));
+    c = sqrt((x3-x2)*(x3-x2) + (y3-y2)*(y3-y2));
 
-    if ((a + b > c) && (b + c > a) && (a + c > b)) {
-sin_ab = (1 - pow(pow(b,2) + pow(c,2) - pow(a,2),2))/(pow(2*b*c,2));
-perimeter = a + b + c;
-pol = (a+b+c)/2;
-area = sqrt(pol*(pol-a)*(pol-b)*(pol-c));
-cout << "perimeter = " << perimeter << endl << "area = " << area << endl;
+    if ((a + b <= c) || (b + c <= a) || (a + c <= b)) {
+	cout << "Error: invalid triangle input" << endl;
         }
-    else
-        cout << "Error: invalid triangle input";
+    else {
+        sin_ab = (1 - pow(pow(b,2) + pow(c,2) - pow(a,2),2))/(pow(2*b*c,2));
+	perimeter = a + b + c;
+	pol = (a+b+c)/2;
+	area = sqrt(pol*(pol-a)*(pol-b)*(pol-c));
+	cout << "perimeter = " << perimeter << endl << "area = " << area << endl;
+	}
     return;
 }
     
 int main()
 {
 	double x, y, radius;
-	double q, w, r, t, j, u, i, o, p, f, g;
+	double q, w, r, t, j, u;
 
 	cout << "Input your coord for circle: ";
 	cin >> x >> y >> radius;
 	cout << endl;
 	cout << "Input your coord for triangle: ";
-	cin >> q >> w >> r >> t >> j >> u >> i >> o >> p >> f >> g;
+	cin >> q >> w >> r >> t >> j >> u;
 	cout << endl;
 	circle(x, y, radius);
-	triangle(q, w, r, t, j, u, i, o, p, f, g);
+	triangle(q, w, r, t, j, u);
+	
 return 0;
 }
 
